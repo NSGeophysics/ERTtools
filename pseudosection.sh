75451639c649b896b3e22d2d7650c907e3a207f4
Alain Plattner <alainplattner@gmail.com>
2017-09-18 15:22:54 -0700

    made figure width a selectable parameter

--- pseudosection.sh
+++ pseudosection.sh
@@ -3,6 +3,7 @@
 # 2) name for output figure
 # 3) min colorbar value [default: min of apparent resistivitites]
 # 4) max colorbar value [default: max of apparent resistivitites]
+# 5) Width in cm [default: 17 cm]
 
 ## Description of input file
 # First column: x positions (centers) of the boxes
@@ -17,10 +18,21 @@
 ### The first part is defining variables and using awk et al
 
 ## Also, set these variables if you like
-width=17c
+
+if [ -z $5 ]
+then
+    width=17c
+else
+    width=$5c
+fi
+
+GMT set PS_MEDIA A0
+
 height=5c
-col=polar
+#col=polar
 #col=jet
+col=ColourMapSuite/lajolla/lajolla.cpt
+#col=ColourMapSuite/davos/davos.cpt
 
 ## Calculating the max and min x and y
 # For min x position and max x position, we want tosubtract and add the rectangle width
@@ -48,7 +60,7 @@ fi
 ### Now comes the GMT part
 ## Make a color scheme.
 # The -Q makes a logarithmically interpolated color scheme
-gmt makecpt -C$col -T${minR}/${maxR}/3 -D -Qo -Z > col.cpt 
+gmt makecpt -C$col -I -T${minR}/${maxR}/3 -D -Qo -Z > col.cpt
 
 ## Plot the rectangles
 gmt psxy $1 -Ccol.cpt -JX$width/-$height -R${minX}/${maxX}/${minY}/${maxY} -Bx1+l"electrode number" -By1 -BN  -Sr -W -K > $2
@@ -57,6 +69,7 @@ gmt psxy $1 -Ccol.cpt -JX$width/-$height -R${minX}/${maxX}/${minY}/${maxY} -Bx1+
 gmt psscale -Dx0c/-0.5c+w${width}/0.4c+h+e -Bx+l"apparent resistivity [@~W@~m]" -Ccol.cpt -O >> $2 
 
 gmt psconvert -A0.1c -Tf -P $2
+
 
 
 ## Remove intermediate files
